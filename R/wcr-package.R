8eee104b5853cd66ca59ab4dade3603e301f6db8
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-04-22 16:03:00 -0400

    Add package file importing dplyr

---
+++ R/wcr-package.R
@@ -0,0 +1,28 @@
+##' Within-cluster resampling
+##'
+##' Implements within-cluster resampling
+##'
+##' @name wcr-package
+##' @aliases wcr-package wcr
+##' @docType package
+##' @import dplyr
+##' @importFrom stats as.formula coef confint mean sd var
+##' @importFrom utils combn
+##' @note Acknowledgement:
+##'
+##' Shunguang Wang for his support in development
+##'
+##' Developmental repository is on github. Your contributions are appreciated.
+##'
+##' \url{https://github.com/kaz-yos/wcr}
+##'
+##' @author Kazuki Yoshida
+##'
+##' Maintainer: Kazuki Yoshida <kazukiyoshida@@mail.harvard.edu>
+##' @seealso
+##' \code{\link{WcrData}}, \code{\link{with.wrc_data}}
+##' @examples
+##'
+##' ## See examples for WcrData and with.wrc_data
+##'
+NULL
