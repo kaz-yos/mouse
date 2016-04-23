12cdb1689cd197c7f52664f29c3ea1114713b119
kaz-yos <kazukiyoshida@mail.harvard.edu>
2016-04-23 15:35:32 -0400

    Add printToggle option to print.mouse()

--- R/print.mouse.R
+++ R/print.mouse.R
@@ -3,9 +3,10 @@
 ##' Shows within-cluster resampling results for a \code{mouse} class object.
 ##'
 ##'
-##' @param object An object that has the \code{mouse} class to be shown.
+##' @param x An object that has the \code{mouse} class to be shown.
+##' @param printToggle whether to print results. Turn off when capturing the invisibly returned matrix.
 ##' @param ... For compatibility with generic. Ignored.
-##' @return None. Results are printed.
+##' @return Matrix of coefficients, standard errors, Z test statistics, and p-values.
 ##' @author Kazuki Yoshida
 ##' @seealso
 ##' \code{\link{mouse}}, \code{\link{with.mouse_data}}
@@ -14,7 +15,7 @@
 ##' ## See examples for mouse and with.mouse_data
 ##'
 ##' @export
-print.mouse <- function(x, ...) {
+print.mouse <- function(x, printToggle = TRUE, ...) {
     cat("Within-cluster resampling results based on", x$Q, "iterations\n")
 
     cat("\nModel formula\n")
@@ -29,6 +30,10 @@ print.mouse <- function(x, ...) {
     resTab <- cbind(resTab,
                     2 * pnorm(abs(resTab[,3]), lower.tail = FALSE))
     colnames(resTab) <- c("Estimate", "Std. Error", "z value", "Pr(|z|>)")
-    print(resTab)
+    ## Print only if printToggle is TRUE
+    if (printToggle) {
+        print(resTab)
+    }
+    ## Return result matrix part invisibly
     invisible(resTab)
 }
